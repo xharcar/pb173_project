85f839090b21132e8df6f3fa64cd876fd8e17fa2
Marek Santa <teslan223@gmail.com>
2015-11-02 16:37:09 +0100

    [MOD] fully functional tank

--- src/tank.cpp
+++ src/tank.cpp
@@ -1,16 +1,55 @@
 #include "tank.h"
+#include <signal.h>
 
 
-int maxFlag, minFlag, sleepMax, sleepMin;
+extern volatile int wasSigUsr2 = 0;
+extern volatile int wasExitSig = 0;
 
+Utils::Utils(int argc, char *argv[]) :
+    mExit(false)
 
-int printError()
 {
-    fprintf(stderr, "Wrong arguments, use -h for help\n");
-    return 1;
+    struct option longopts[] = {
+            { "area-size",     required_argument, NULL, 'a' },
+            { "help",          no_argument,       NULL, 'h' },
+            { 0, 0, 0, 0 }
+        };
+        int option_index = 0;
+        int c;
+        while ((c = getopt_long(argc, argv, "a:h", longopts, NULL)) != -1) {
+            switch (c) {
+            case 'a':
+                this->mMapWidth = atoi(argv[optind-1]);
+                this->mMapHeight = atoi(argv[optind]);
+                break;
+            case 'h':
+                this->printHelp();
+                exit(0);
+            default:
+                this->printError();
+                exit(-1);
+            }
+        }
+        // printf("X: %i, Y: %i\n", areaX, areaY);
+        /* we need these opts */
+        if(mMapHeight<=0 || mMapWidth<=0) {
+           this->printHelp();
+            exit(-1);
+        }
+}
+Utils::~Utils()
+{
+
 }
 
-int printHelp()
+
+void Utils::printError()
+{
+    fprintf(stderr, "Wrong arguments, use -h for help\n");
+    exit(-1);
+}
+
+void Utils::printHelp()
 {
     printf("=====================================================\n");
     printf("|         PB173 Internet Of Tanks presents:         |\n");
@@ -20,55 +59,155 @@ int printHelp()
     printf("|           / / / ___ |/ /|  / /| |                 |\n");
     printf("|          /_/ /_/  |_/_/ |_/_/ |_|                 |\n");
     printf("-----------------------------------------------------\n");
-    printf("                    USAGE                            \n");
-    printf("  -h | --help       Show this help                   \n");
-    printf("  --leep-min [n]    set min sleep time in secs       \n");
-    printf("  --leep-max [n]    set max sleep time in secs       \n");
+    printf("                        USAGE                            \n");
+    printf("  -h | --help           Show this help                   \n");
+    printf("  --area-size [n]x[m]   size of area   \n");
     printf("=====================================================\n");
 
-    return 1;
+    ;
 }
 
-
-int parseArgs(int argc, char *argv[])
+Tank::Tank(Utils *utils) :
+    mUtils(utils),
+    lastCommandSuccess(true),
+    wasLastMove(false) //temporary
 {
-    struct option longopts[] = {
-       { "sleep-max",     required_argument,       NULL,  'v'   },
-       { "sleep-min",    required_argument, NULL,   'm' },
-       { "help",    no_argument,       NULL,    'h' },
-      { 0, 0, 0, 0 }
-    };
-    int c;
-    while ((c = getopt_long(argc, argv, "v:m:h", longopts, NULL)) != -1) {
-        switch (c) {
-        case 'v':   sleepMax = atoi(optarg);
-            maxFlag = 1;
-            break;
-        case 'm':   sleepMin = atoi(optarg);
-            minFlag = 1;
-            break;
-        case 'h':  return printHelp();
-            break;
-        default:  return printError();
-        }
+    srand(time(NULL));
+}
 
+Tank::~Tank()
+{
+    delete this->mUtils;
+}
+
+void Tank::waitForSignal()
+{
+    pause();    //waiting
+    if(wasSigUsr2 == 1)
+    {
+        wasSigUsr2 = 0;
+        this->nextMove();
+    }
+    else if(wasExitSig == 1)
+    {
+        delete this;
+        exit(0);
     }
 
-    if(!(maxFlag && minFlag))   //we need both opts
-        return printError();
-    return 0;
+}
+
+void Tank::nextMove()
+{
+    //as completly different process i don't know my initial position
+    //so i am going to fire everywhere BADAAAAAAAAA
+    //and move randomly to test ncurses
+    switch (this->lastCommand)
+    {
+    case FIRE_UP:
+        this->lastCommand = FIRE_RIGHT;
+        break;
+    case FIRE_RIGHT:
+        this->lastCommand = FIRE_DOWN;
+        break;
+    case FIRE_DOWN :
+        this->lastCommand = FIRE_LEFT;
+        break;
+    default :
+        this->lastCommand = FIRE_UP;
+    }
+
+    if (this->wasLastMove)
+    {
+        this->wasLastMove = false;
+    }
+    else
+    {
+        this->wasLastMove = true;
+        this->lastCommand = (Command)(rand() % 4);
+    }
+
+    this->sendCommand(this->lastCommand);
+
+
+}
+
+bool Tank::sendCommand(Command command)
+{
+    switch (command)
+    {
+    case Command::MOVE_UP :
+        this->commandToSend = this->moveUp;
+        break;
+    case Command::MOVE_DOWN :
+        this->commandToSend = this->moveDown;
+        break;
+    case Command::MOVE_LEFT :
+        this->commandToSend = this->moveLeft;
+        break;
+    case Command::MOVE_RIGHT :
+        this->commandToSend = this->moveRight;
+        break;
+    case Command::FIRE_UP :
+        this->commandToSend = this->fireUp;
+        break;
+    case Command::FIRE_DOWN :
+        this->commandToSend = this->fireDown;
+        break;
+    case Command::FIRE_LEFT :
+        this->commandToSend = this->fireLeft;
+        break;
+    case Command::FIRE_RIGHT :
+        this->commandToSend = this->fireRight;
+        break;
+    }
+
+    if (write(STDOUT_FILENO, this->commandToSend, 2) < 2)
+    {
+        std::cerr << "something went wrong while writing to pipe (too lazy to syslog)" << std::endl;
+        return false;
+    }
+    return true;
+}
+
+void signal_handler(int sig)
+{
+    switch(sig)
+    {
+    case SIGUSR2:
+        wasSigUsr2 = 1;
+        break;
+    case SIGINT :
+    case SIGTERM:
+    case SIGQUIT:
+        wasExitSig = 1;
+        break;
+    default:
+        break;
+    }
 }
 
 int main(int argc, char *argv[])
 {
-    if(parseArgs(argc, argv) == 1)  //parse arguments
-        return 1;
+    struct sigaction action;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+    action.sa_handler = signal_handler;
+    sigaction(SIGINT, &action, NULL);
+    sigaction(SIGTERM, &action, NULL);
+    sigaction(SIGQUIT, &action, NULL);
+    sigaction(SIGUSR2, &action, NULL);
 
-    srand(time(NULL));
-    int sleepSecs;
-    sleepSecs = rand() % (sleepMax-sleepMin) + sleepMin;
 
-    usleep(sleepSecs*1000000);
+    Utils* utils = new Utils(argc, argv);
+    Tank * tank = new Tank(utils);
+
+    while(true)
+    {
+        tank->waitForSignal();
+    }
+
+
+
 
     return 0;
 }
--- src/tank.h
+++ src/tank.h
@@ -2,3 +2,57 @@
 
 #include "config.h"
 #include <time.h>
+#include <iostream>
+
+class Utils {
+    int mMapHeight;
+    int mMapWidth;
+    bool mExit;
+
+public:
+    Utils(int argc, char *argv[]);
+    ~Utils();
+    void printHelp();
+    void printError();
+
+    int getMapHeight() {return this->mMapHeight; }
+    int getMapWidth() {return this->mMapWidth; }
+    bool getExit() {return this->mExit; }
+
+
+
+};
+
+class Tank {
+public:
+    enum Command {
+        MOVE_UP = 0, MOVE_DOWN = 1, MOVE_LEFT = 2, MOVE_RIGHT = 3, FIRE_UP = 4, FIRE_DOWN = 5, FIRE_LEFT = 6, FIRE_RIGHT = 7
+    } ;
+
+
+    Tank(Utils *utils);
+    ~Tank();
+    void waitForSignal();
+    void nextMove();
+    bool sendCommand(Command command);
+
+    const char * commandToSend;
+    const char moveUp[2] = {'m', 'u'};
+    const char moveDown[2] = {'m', 'd'};
+    const char moveLeft[2] = {'m', 'l'};
+    const char moveRight[2] = {'m', 'r'};
+    const char fireUp[2] = {'f', 'u'};
+    const char fireDown[2] = {'f', 'd'};
+    const char fireLeft[2] = {'f', 'l'};
+    const char fireRight[2] = {'f', 'r'};
+
+
+private:
+    Utils *mUtils;
+    Command lastCommand;
+    bool wasLastMove;
+    bool lastCommandSuccess;
+
+    int mWidthPos;
+    int mHeightPos;
+};
