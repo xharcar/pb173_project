0885e02493a130217ce016bf1b2e92a5bc80a4e5
Neel <Neel012@users.noreply.github.com>
2015-11-02 18:36:27 +0100

    [MOD] [worldclient] Moved method declarationas to .cpp file

--- src/worldclient.h
+++ src/worldclient.h
@@ -36,66 +36,15 @@ public:
         open_pipe(pipe);
     }
 
-    void get_pid() {
-        ifstream pid_file;
-        pid_file.open("/var/run/world.pid");
-        if (pid_file) {
-            cerr << "world.pid file does not exist. World process is not running." << endl;
-            exit(-1);
-        } else if ( pid_file >> world_pid ) {
-            /* Successfully read the number  */
-        } else {
-            /* Failed to read the number */
-            cerr << "Failed to read the pid from the world.pid file." << endl;
-            exit(-1);
-        }
-    }
-
-    void open_pipe(char * pipe) {
-        int fd;
-        if ( (fd = open(pipe, O_RDONLY) ) < 0 ) {
-            cerr << strerror(errno) << "Can not open the pipe for streaming data from world process." << endl;
-            exit(-1);
-        }
-        pipe_stream = fdopen(fd, "r");
-        if (!pipe_stream) {
-            cerr << strerror(errno) << "Can not open the pipe for streaming data from world process." << endl;
-            exit(-1);
-        }
-        clearerr(pipe_stream);
-        int dimensions = fscanf(pipe_stream, "%d, %d", &width, &height);
-        if ( dimensions == EOF && ferror(pipe_stream) ) {
-            cerr << strerror(errno) << "Error occured while parsing the pipe stream." << endl;
-        } else if (dimensions != 2 ) {
-            cerr << "Error: Worng format of the data in the pipe." << endl;
-            exit(-1);
-        }
-    }
+    void get_pid();
+    void open_pipe(char * pipe);
 };
 
 class NCursesClient  : public WorldClient {
     WINDOW * nc_world;
     // WINDOW * nc_stats;
 public:
-    NCursesClient(char * pipe) : WorldClient(pipe) {
-        initscr();
-        start_color();
-        /* Create color associations in ncurses */
-        init_pair(Color::RED, COLOR_RED, COLOR_RED);
-        init_pair(Color::GREEN, COLOR_GREEN, COLOR_GREEN);
-        /* Hide the cursor in ncurses */
-        curs_set(0);
-        /* Disables line buffering */
-        cbreak();
-        noecho();
-
-        /* Add padding for borders */
-        nc_world = newwin(height + 2, width + 2, 0, 0);
-        // nc_stats = newwin(10, 20, 1, width + 2 + 3);
-        box(nc_world, 0, 0);
-        wrefresh(nc_world);
-        keys();
-    }
+    NCursesClient(char * pipe);
 
     ~NCursesClient() {
         delwin(nc_world);
@@ -103,63 +52,13 @@ public:
         endwin();
     }
 
-    void print_tanks() {
-        char sector;
-        int x = 0, y = 0;
-        while ( fscanf(pipe_stream, ",%c", &sector) != EOF) {
-            switch (sector) {
-            case 'r':
-                draw_tank(x, y, Color::RED);
-                break;
-            case 'g':
-                draw_tank(x, y, Color::GREEN);
-                break;
-            case '0':
-                undraw_tank(x, y);
-                break;
-            }
-            x++;
-            if (x >= width) {
-                x = 0;
-                y++;
-            }
-            keys();
-        }
-        wrefresh(nc_world);
-    }
+    void print_tanks();
 
-    void keys() {
-        char c = (char)getch();
-        switch (c) {
-            case 'x':
-                kill(world_pid, SIGINT);
-                break;
-            case 'r':
-                kill(world_pid, SIGUSR1);
-                break;
-            case 'q':
-                exit(0);
-                break;
-        }
-    }
+    void keys();
 
-    void draw_tank(int x, int y, Color color) {
-        wattrset(nc_world, COLOR_PAIR(color));
-        /* Compensate for border padding */
-        mvwaddch(nc_world, y + 1, x + 1, ACS_BLOCK);
-        /* type cast enum class color */
-        wattroff(nc_world, COLOR_PAIR(color));
-        wrefresh(nc_world);
-    }
-
-    void undraw_tank(int x, int y) {
-        /* COLOR_PAIR(0) sets the default color */
-        wattrset(nc_world, COLOR_PAIR(0));
-        /* Compensate for border padding */
-        mvwaddch(nc_world, y + 1, x + 1, ' ');
-        wattroff(nc_world, COLOR_PAIR(0));
-        wrefresh(nc_world);
-    }
+    void draw_tank(int x, int y, Color color);
+    
+    void undraw_tank(int x, int y);
 
     /*
     void refresh_stats(int green_kills, int red_kills) {
